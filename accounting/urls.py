3b440667a584547b33ce1825028d0ab923d736e6
Maksim Legeyda <maksim.legeyda@yandex.ru>
2025-10-09 22:01:25 +0300

    HOTFIX

--- accounting/urls.py
+++ accounting/urls.py
@@ -6,9 +6,9 @@ from .views.transaction_views import TransactionViewSet
 from .views.wallet_views import WalletViewSet
 
 router = DefaultRouter()
-router.register(r'transactions', TransactionViewSet)
-router.register(r'categories', TransactionCategoryViewSet)
-router.register(r'wallets', WalletViewSet)
+router.register(r'transactions', TransactionViewSet, basename='transaction')
+router.register(r'categories', TransactionCategoryViewSet, basename='category')
+router.register(r'wallets', WalletViewSet, basename='wallet')
 
 urlpatterns = [
     path('', include(router.urls)),
